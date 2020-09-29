0591918f446ddeb646fc8c490daad34a2731e16e
mfaiza <mfaizaschool@gmail.com>
2020-09-29 21:19:56 +0700

    add partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.order("created_at ASC")
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -23,11 +24,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-       @article = Article.find(params[:id]) 
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was updated succesfully"
             redirect_to @article
@@ -37,7 +36,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         if @article.destroy
             flash[:notice] = "Article was Deleted succesfully"
             redirect_to articles_path
@@ -47,5 +45,9 @@ class ArticlesController < ApplicationController
     private
         def article_params
             params.require(:article).permit(:title, :body)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
         end
 end
