30d1e08289b57afc7dda3222c783bebea8b4b7b3
mfaiza <mfaizaschool@gmail.com>
2020-11-02 06:51:30 +0700

    adding state before_save and change the author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, uniqueness: { case_sensitive: false }, 
                         presence: true, 
