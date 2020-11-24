c46a80b00307c066acef26cbf85bdf3909a7f3a3
Mochamad Faiz Alamsyah <mochfaizalamsyah@gmail.com>
2020-11-24 21:06:31 +0700

    add admin field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_012340) do
+ActiveRecord::Schema.define(version: 2020_11_24_131036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_012340) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
